b012de93cef891ab8de628b266c41fdbe3c8082b
Atishay Jain <hritikjain707@gmail.com>
2023-05-06 08:36:33 +0530

    Feat: Add Razorpay checkout

--- src/Components/Cart/cartTotal.js
+++ src/Components/Cart/cartTotal.js
@@ -1,40 +1,64 @@
-import { useState } from "react";
-import { useCart } from "../Context/CartContext";
+import { useEffect, useState } from 'react';
+import { useCart } from '../Context/CartContext';
 
 export default function CartTotal() {
   const { state, dispatch } = useCart();
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [checkInput, setCheckInput] = useState('');
 
-  const [checkInput, setCheckInput] = useState("");
+  const totalCartPrice = state.cart.reduce((acc, cur) => {
+    return acc + parseFloat(cur.price) * cur.qty;
+  }, 0);
 
-  let priceArr = [];
-  let qtyArr = [];
-  let totalPrice = 0;
+  const itemQty = state.cart.reduce((acc, cur) => {
+    return acc + cur.qty;
+  }, 0);
 
-  state.cart.map(
-    (item) => (priceArr = [...priceArr, parseFloat(item.price) * item.qty])
-  );
-  state.cart.map((item) => (qtyArr = [...qtyArr, item.qty]));
+  const options = {
+    key: process.env.REACT_APP_RAZORPAY_TEST_KEY,
+    amount: totalPrice * 100,
+    currency: 'INR',
+    name: 'Dice Pizza',
+    description: 'Test Transaction',
+    image: 'https://example.com/your_logo',
+    callback_url: 'https://eneqd3r9zrjok.x.pipedream.net/',
+    prefill: {
+      name: 'Guest User',
+      email: 'guest.user@example.com',
+      contact: '9000090000'
+    },
+    notes: {
+      address: 'Razorpay Corporate Office'
+    },
+    theme: {
+      color: '#78bc61'
+    }
+  };
 
-  let totalCartPrice = priceArr.reduce((a, b) => a + b, 0);
+  const openCheckout = () => {
+    // eslint-disable-next-line no-undef
+    var rzp1 = new Razorpay(options);
+    rzp1.open(options);
+  };
 
-  let itemQty = qtyArr.reduce((a, b) => a + b, 0);
-
-  totalPrice =
-    totalCartPrice < state.discount.discountAmt
-      ? totalCartPrice
-      : totalCartPrice - state.discount.discountAmt;
+  useEffect(() => {
+    setTotalPrice(() => {
+      return totalCartPrice < state.discount.discountAmt
+        ? totalCartPrice
+        : totalCartPrice - state.discount.discountAmt;
+    });
+  }, [state.discount.discountAmt, totalCartPrice]);
 
   return (
     <div className="cartTotalContainer">
-      {/* <h2> I am cart Total</h2> */}
       <div className="cartTotalItems">
         <h3 className="cartTotalHeading">
-          Price Details ({itemQty} {itemQty > 1 ? "items" : "item"})
+          Price Details ({itemQty} {itemQty > 1 ? 'items' : 'item'})
         </h3>
         <h6 className="cartTotalPrice">Total MRP : ₹ {totalCartPrice}</h6>
         <h6 className="cartTotalDiscount">
-          Discount : ₹{" "}
-          {state.discount.giveDiscount ? state.discount.discountAmt : "00"}
+          Discount : ₹{' '}
+          {state.discount.giveDiscount ? state.discount.discountAmt : '00'}
         </h6>
         {checkInput && (
           <p className="cartTotalMessage">{state.discount.discountMsg}</p>
@@ -50,8 +74,8 @@ export default function CartTotal() {
           className="cartTotalInputBtn"
           onClick={() =>
             dispatch({
-              type: "HANDLE_DISCOUNT",
-              payload: { cartPrice: totalCartPrice, couponInput: checkInput },
+              type: 'HANDLE_DISCOUNT',
+              payload: { cartPrice: totalCartPrice, couponInput: checkInput }
             })
           }
         >
@@ -65,7 +89,9 @@ export default function CartTotal() {
       <div className="cartTotal">
         <h4 className="cartTotalAmt">Total Amount: ₹ {totalPrice}</h4>
       </div>
-      <button className="cartTotalBtn">Place Order</button>
+      <button className="cartTotalBtn" onClick={openCheckout}>
+        Place Order
+      </button>
     </div>
   );
 }
